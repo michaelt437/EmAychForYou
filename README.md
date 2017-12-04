b9b8d4756ec43596358b3f31954ca25277997503
michaelt437 <michaelt437@gmail.com>
2017-12-03 21:08:58 -0500

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# mh4u-ass-js
+
+> MH4U ASS Js
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img src="./assets/mh4u.png">
+    <pre>{{chest}}</pre>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import head_gear from './assets/armor/headgear'
+import chest from './assets/armor/chest'
+import arms from './assets/armor/arms'
+import waist from './assets/armor/waist'
+import legs from './assets/armor/legs'
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      chest: chest
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
