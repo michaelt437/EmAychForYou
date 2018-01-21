b2c015b350028836ed1203635705d1218c270c33
michaelt437 <michaelt437@gmail.com>
2018-01-20 21:32:08 -0500

    changed to unstyled lists

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import styles from './assets/styles/styles.scss'
 import Vue from 'vue'
 import App from './App'
 
